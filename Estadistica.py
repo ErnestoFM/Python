86171da3ccee955b830066388037b2a2ecc2ce1c
Enresto <ernesto.fierro@gmail.com>
2022-02-15 20:38:20 -0600

    first commit

---
+++ Estadistica.py
@@ -0,0 +1,23 @@
+valor=False
+def validar(valor):
+    for x 
+
+def sacar_media(lista):
+    largo=len(lista)
+    if largo%2==0:
+        indice2=(largo/2)+1
+        media=(lista[largo/2]+lista[(largo/2)+1])/2
+        return media
+    else:
+        return lista[largo/2]
+milista=[]
+def main():
+    milista=input("Pasame una lista de números que quieres sacarle las medidas \
+        de tendencia central \n")
+    print(milista)
+    desicion=input("¿Esta es la lista que quieres que calcular?")
+    if desicion == 'si':
+        break
+
+if __name__=='__main__':
+    sacar_media(milista)
